c5b7a276eb05ade0364ecf45382b6c77a29c860b
tusharrp <112104765+tusharrp@users.noreply.github.com>
2024-02-19 22:33:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // Set a timeout to open a new page after 5 seconds (adjust the time as needed)
     setTimeout(function() {
         // Replace the absolute path with the full file:// URL
-        window.location.href = 'https://github.com/tusharrp/Vaid/blob/d73e2e10b0343038e89c5ae6c8d682a17d9047bb/2nd/index.html';
+        window.location.href = '../2nd/index.html';
     }, 5000); // 5000 milliseconds = 5 seconds
 });
