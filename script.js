e9001da740aaf9a3560cd0f34cc410e02098b483
tusharrp <112104765+tusharrp@users.noreply.github.com>
2024-02-19 22:14:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Wait for the DOM to be fully loaded
+
+    // Set a timeout to open a new page after 5 seconds (adjust the time as needed)
+    setTimeout(function() {
+        // Replace the absolute path with the full file:// URL
+        window.location.href = '../2nd/index.html';
+    }, 5000); // 5000 milliseconds = 5 seconds
+});
